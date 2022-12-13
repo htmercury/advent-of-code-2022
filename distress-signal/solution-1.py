c6857d8426b2d7e117248eebd2b2db0a22dfd8f5
Ka Wong <kawong2@WKMZTF6825A4.global.publicisgroupe.net>
2022-12-13 02:14:12 -0600

    solution for day 13

---
+++ distress-signal/solution-1.py
@@ -0,0 +1,46 @@
+import ast
+
+input_file = open('distress-signal/input.txt', 'r')
+signal_lines = input_file.readlines()
+
+def get_pairs():
+    pairs = []
+    curr_pair = []
+    for signal in signal_lines:
+        signal = signal.strip('\n')
+        if len(signal) != 0:
+            curr_pair.append(ast.literal_eval(signal))
+        
+        if len(curr_pair) == 2:
+            pairs.append(curr_pair)
+            curr_pair = []
+    
+    return pairs
+
+def compare_values(value_one, value_two):
+    if isinstance(value_one, int) and isinstance(value_two, int):
+        return value_one - value_two
+    elif isinstance(value_one, list) and isinstance(value_two, list):
+        min_length = min(len(value_one), len(value_two))
+        for i in range(min_length):
+            diff = compare_values(value_one[i], value_two[i])
+            if diff != 0:
+                return diff
+        # compare list lengths if no evaluation
+        return len(value_one) - len(value_two)
+    elif isinstance(value_one, int) and isinstance(value_two, list):
+        return compare_values([value_one], value_two)
+    else:
+        return compare_values(value_one, [value_two])
+
+def solution():
+    pairs = get_pairs()
+    result = 0
+    for i, pair in enumerate(pairs, start=1):
+        value_one, value_two = pair
+        if compare_values(value_one, value_two) < 0:
+            result += i
+
+    return result
+
+print(solution())
